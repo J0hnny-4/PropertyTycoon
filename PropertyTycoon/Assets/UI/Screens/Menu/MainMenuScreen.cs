6269922a6537d89a6b64bd5ab92aae8e93177677
ella <cb792@sussex.ac.uk>
2025-02-25 15:12:39 +0000

    one

--- PropertyTycoon/Assets/UI/Screens/Menu/MainMenuScreen.cs
+++ PropertyTycoon/Assets/UI/Screens/Menu/MainMenuScreen.cs
@@ -6,22 +6,26 @@ namespace UI.Screens.Menu
     public class MainMenuScreen : BaseScreen
     {
         private Button _startButton;
+        private Button _settingButon;
         private Button _quitButton;
 
         public override void Initialise()
         {
             // get reference to UI elements
             _startButton = Root.Q<Button>("start-button");
+            _settingButon = Root.Q<Button>("setting-button");
             _quitButton = Root.Q<Button>("quit-button");
             
             // register button actions
             _startButton.RegisterCallback<ClickEvent>(OnStartClicked);
+            _settingButon.RegisterCallback<ClickEvent>(OnSettingsClicked);
             _quitButton.RegisterCallback<ClickEvent>(OnQuitClicked);
         }
         
         protected override void CleanUp()
         {
             _startButton.UnregisterCallback<ClickEvent>(OnStartClicked);
+            _settingButon.UnregisterCallback<ClickEvent>(OnSettingsClicked);
             _quitButton.UnregisterCallback<ClickEvent>(OnQuitClicked);
         }
 
@@ -31,10 +35,18 @@ namespace UI.Screens.Menu
             UIManager.NavigateTo<GameModeScreen>();
         }
 
+        private void OnSettingsClicked(ClickEvent e){
+            Debug.Log("Settings clicked"); 
+            UIManager.NavigateTo<SettingsScreen>();
+        }
+
         private void OnQuitClicked(ClickEvent e)
         {
             Debug.Log("QUIT clicked");
             Application.Quit();
         }
+
+        
+
     }
 }
---
+++ PropertyTycoon/Assets/UI/Screens/Menu/SettingsModeScreen.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI.Screens.Menu
+{
+    public class SettingsScreen : BaseScreen
+    {
+        public override void Initialise()
+        {
+            //hello
+        }
+        protected override void CleanUp()
+        {
+            //test
+        }
+
+    }
+
+
+
+
+
+
+
+}
