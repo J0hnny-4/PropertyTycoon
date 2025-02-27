404a7d1febb8d052b84edac347b4dea27a679d4e
Jacopo Calvi <59688601+cactuuus@users.noreply.github.com>
2025-02-27 20:54:34 +0000

    base setup
    - game scene
    - game UI manager
    - main game screen (script & uxml)

---
+++ PropertyTycoon/Assets/Scripts/UI/Game/MainGameScreen.cs
@@ -0,0 +1,17 @@
+namespace UI.Game
+{
+    public class MainGameScreen : BaseScreen<GameScreen>
+    {
+        public override void Initialise()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void CleanUp()
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        
+    }
+}
