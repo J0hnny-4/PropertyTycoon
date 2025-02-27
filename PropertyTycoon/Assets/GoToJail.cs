7f9260a2f348df2ae6e266e11b901ca630cff18a
Patrick Culmer <125742590+VeryCoolPatrick@users.noreply.github.com>
2025-02-27 12:04:54 +0000

    Added most comments except constructors

--- PropertyTycoon/Assets/GoToJail.cs
+++ PropertyTycoon/Assets/GoToJail.cs
@@ -1,7 +1,13 @@
+/// <summary>
+/// Upon landing the player is sent to jail.
+/// </summary>
 public class GoToJail : Square
 {
     public GoToJail(string name) : base(name) { }
-
+    
+    /// <summary>
+    /// Sends the player to jail.
+    /// </summary>
     public override void playerLands()
     {
         GameState.currentPlayer.goToJail();
--- PropertyTycoon/Assets/HumanPlayer.cs
+++ PropertyTycoon/Assets/HumanPlayer.cs
@@ -1,3 +1,8 @@
+/// <summary>
+/// A human player that selects a piece and makes moves manually.
+/// Choices are made through menu options.
+/// Game rules function normaly.
+/// </summary>
 public class HumanPlayer : Player
 {
     public HumanPlayer(string name, string piece) : base(name, piece) { }
