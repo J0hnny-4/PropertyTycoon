a66c02925f0f20f870b83ac53434ccc361804b80
GibGibSag <gibousagnia@outlook.com>
2025-03-17 22:59:04 +0000

    Added List of Objects, Object creations and got it working in Unity. Needs cleaning up later

---
+++ PropertyTycoon/Assets/Scripts/Parser/Parser.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Xml;
+using Data;
+
+
+public class Parser
+{
+  private static string[,] data;
+    private static DataTable LoadDataTableFromXml(string path)
+    {
+      //Creates object xml and loads the path passed through the initial call
+      XmlDocument xml = new XmlDocument();
+      xml.Load(path);
+      //Creates a dataset and store the Xml contents into dataSet
+      DataSet dataSet = new DataSet();
+      dataSet.ReadXml(new StringReader(xml.InnerXml));
+      return dataSet.Tables[0];
+    }
+
+    public static void Parse()
+    {
+      DataTable propertyList = LoadDataTableFromXml("Assets/Resources/Data/PropertyTable.xml");
+      //Sets the length of the array depending on the attributes loaded
+      data = new string[propertyList.Rows.Count,propertyList.Columns.Count];
+      int row = 0;
+      //Loop to store the data into the 2d array
+      foreach(DataRow dataRow in propertyList.Rows)
+      {
+        for(int col = 0; col < dataRow.ItemArray.Count(); col++)
+        {
+          data[row, col] = dataRow[col].ToString();
+          //Test
+          Console.Write("{0} ", data[row, col]);
+        }
+        row++;
+        //Test
+        Console.Write(Environment.NewLine + Environment.NewLine);
+      }
+      
+    }
+
+    public static void TileCreator(){
+      Parse();
+      List<SquareData> tiles = new List<SquareData>();
+      int[] rentlist = new int[2];
+      
+      for(int i = 0; i < 39; i++)
+      {
+        int cost = 0;
+        int rent = 0;
+        string name = data[i, 1];
+        int housecost = 0;
+        int temp = 0;
+
+        int.TryParse(data[i, 5], out cost);
+        //var cost = Convert.ToInt32(data[i, 5]);
+        //var rent = Convert.ToInt32(data[i, 6]);      
+        int.TryParse(data[i, 6], out rent);
+        //var housecost = Convert.ToInt32(data[i, 6]); 
+        int.TryParse(data[i, 6], out housecost);
+        
+        int x = 0;
+        switch (data[i, 2])
+        {
+          case("Action"):
+            tiles.Add(new OwnableData(name,cost));            
+            break;
+          
+          case("Brown" or "Blue" or "Green" or "Red" or "Yellow" or "Purple" or "Deep Blue" or "Orange"):
+            x = 0;
+            int y = 6;
+            while (x<3){
+              int.TryParse(data[i, y], out temp);
+              rentlist[x] = temp;
+              x++;
+              y++;
+            }
+            tiles.Add(new PropertyData(name,cost, rentlist,data[i,2],housecost));
+            break;
+          
+          case("Utilities"):
+            x = 0;
+            y = 6;
+            while (x<3){
+              int.TryParse(data[i, y], out temp);
+              rentlist[x] = temp;
+              x++;
+              y++;
+            }
+            break;
+          
+          case("Got to Jail"):
+            tiles.Add(new OwnableData(name,cost));
+            break;
+          
+          case("Take card"):
+            tiles.Add(new OwnableData(name,cost));
+            break;
+          
+          case("Station"): 
+            tiles.Add(new StationData(name, cost, rent));
+            break;
+        }
+      }
+    }
+
+  } 
