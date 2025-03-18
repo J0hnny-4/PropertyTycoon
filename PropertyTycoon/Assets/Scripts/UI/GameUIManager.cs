16299f2630ea2b2c939c3939b8e0f298ccde0235
Jacopo Calvi <59688601+cactuuus@users.noreply.github.com>
2025-03-18 15:00:43 +0000

    Removed redundant code & updated DialogBoxFactory namespace

--- PropertyTycoon/Assets/Scripts/UI/GameUIManager.cs
+++ PropertyTycoon/Assets/Scripts/UI/GameUIManager.cs
@@ -26,29 +26,5 @@ namespace UI
             _mainGameScreen = (MainGameScreen)baseScreen;
             _mainGameScreen.Show();
         }
-        
-        /// <summary>
-        /// Shows an dialog box prompting the player to pay.
-        /// </summary>
-        /// <param name="square">The type of Square associated with this interaction</param>
-        /// <param name="amount">Amount to pay.</param>
-        /// <returns>True once the player presses the pay button.</returns>
-        public static Task<bool> PayFeeDialogBox(Square square, int amount)
-        {
-            return DialogBoxFactory.MakePaymentDialogBox(square, amount).AsTask();
-        }
-
-        /// <summary>
-        /// Shows a dialog box prompting the player to choose between staying in prison or pay their bail. 
-        /// </summary>
-        /// <returns>True if the player decides to pay, False if they decide to stay in prison.</returns>
-        public static Task<bool> JailLandingDialogBox()
-        {
-            // todo add option for card?
-            // todo disable pay button if insufficient funds?
-            return DialogBoxFactory.MakeJailLandingDialogBox().AsTask();
-        }
-        
-        
     }
 }
