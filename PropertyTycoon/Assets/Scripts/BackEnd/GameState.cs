861b777cb6674f0dc1cb5c37daa29502c06e06ab
ella <cb792@sussex.ac.uk>
2025-03-20 12:06:44 +0000

    Board with players and game ui

--- PropertyTycoon/Assets/Scripts/BackEnd/GameState.cs
+++ PropertyTycoon/Assets/Scripts/BackEnd/GameState.cs
@@ -40,8 +40,6 @@ namespace BackEnd
         /// </summary>
         private GameState()
         {
-            var _allTokens = Resources.Load<Token>("Tokens/cat");
-            _players.Add(new PlayerData("test",_allTokens, false) );
             for(int i = 0; i < 40; i++)
             {
                 if(i == 0) _board.Add(new SquareData("Go"));
