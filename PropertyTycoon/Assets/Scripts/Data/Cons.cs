0b62610f15ef6478393bb52230e60bbd7dad836c
Jacopo Calvi <59688601+cactuuus@users.noreply.github.com>
2025-04-08 14:00:05 +0100

    fixes small bugs

--- PropertyTycoon/Assets/Scripts/Data/Cons.cs
+++ PropertyTycoon/Assets/Scripts/Data/Cons.cs
@@ -20,7 +20,7 @@ namespace Data
         public const int HotelCostMultiplier = 5;
         public const int IncomeTax = 200;
         public const int SuperTax = 400;
-        public const int DoublesToJail = 3;
+        public const int DoublesToJail = 2;
         public static readonly int[] UtilitiesMultiplier = { 4, 10 };
         public static readonly int[] StationsRent = { 25, 50, 100, 200 };
     }
